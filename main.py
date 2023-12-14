fb7b65b4e5b8e451f3c77960b60c56b82538fbd5
Vo Ngoc Sang <sangngoc27042001@gmail.com>
2023-12-14 14:38:49 +0700

    hello

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from typing import Union
 from fastapi.staticfiles import StaticFiles
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
-
+handler = Mangum(app=app)
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"Hello": "World"}
 
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
+async def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
